02313dda9ae1d209256995e067679d5815a40aed
hilodfulaidien <hilodfulaidien@tutanota.com>
2023-03-09 10:05:17 +0800

    t

---
+++ src/routes/AlbumDetailPanel.tsx
@@ -0,0 +1,27 @@
+export default function AlbumDetailPanel() {
+
+  return (
+    <div className="my-album-detail-panel">
+      <div className="my-album-detail-panel-header">
+        <div className="my-album-detail-panel-thumbnail">
+          <img />
+        </div>
+        <div className="my-album-detail-panel-info">
+          <div className="my-album-detail-panel-name">
+            <span></span>
+          </div>
+          <div className="my-album-detail-panel-artist">
+            <span></span>
+          </div>
+          <div className="my-album-detail-panel-action">
+
+          </div>
+        </div>
+      </div>
+
+      <div className="my-album-detail-panel-content">
+        
+      </div>
+    </div>
+  );
+}
