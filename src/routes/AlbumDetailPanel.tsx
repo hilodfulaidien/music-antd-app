34d441e9da584afbff3a060795076e9c5c13a0f2
hilodfulaidien <hilodfulaidien@tutanota.com>
2023-03-09 16:21:30 +0800

    t

--- src/routes/AlbumDetailPanel.tsx
+++ src/routes/AlbumDetailPanel.tsx
@@ -1,5 +1,7 @@
-export default function AlbumDetailPanel() {
+import { Table } from "antd";
+import { ColumnsType } from "antd/es/table";
 
+export default function AlbumDetailPanel() {
   return (
     <div className="my-album-detail-panel">
       <div className="my-album-detail-panel-header">
@@ -13,15 +15,52 @@ export default function AlbumDetailPanel() {
           <div className="my-album-detail-panel-artist">
             <span></span>
           </div>
-          <div className="my-album-detail-panel-action">
-
-          </div>
+          <div className="my-album-detail-panel-action"></div>
         </div>
       </div>
 
       <div className="my-album-detail-panel-content">
-        
+        <Table columns={columns} dataSource={data} showHeader={true} />
       </div>
     </div>
   );
 }
+
+
+
+interface TrackDataType {
+  //key: React.Key;
+  trackNumber: number;
+  trackName: string;
+  trackArtist: string[];
+  length: number;
+}
+
+const columns: ColumnsType<TrackDataType> = [
+  {
+    title: "#",
+    dataIndex: "trackNumber",
+  },
+  {
+    title: "Title",
+    dataIndex: "trackName",
+  },
+  {
+    title: "Artist",
+    dataIndex: "trackArtist",
+  },
+  {
+    title: "length",
+    dataIndex: "length",
+  },
+];
+
+const data: TrackDataType[] = [
+  {
+    trackNumber: 1,
+    trackName: "string",
+    trackArtist: ['string','string',],
+    length: 45,
+  },
+];
+
--- src/routes/PlayQueuePanel.tsx
+++ src/routes/PlayQueuePanel.tsx
@@ -1,7 +1,35 @@
+import Table, { ColumnsType } from "antd/es/table";
 
 
 export default function PlayQueuePanel(){
     return (
-        <></>
+        <Table />
     )
 }
+interface TrackDataType {
+    //key: React.Key;
+    trackNumber: number;
+    trackName: string;
+    trackArtist: string[];
+    length: number;
+  }
+  
+  const columns: ColumnsType<TrackDataType> = [
+    {
+      title: "#",
+      dataIndex: "trackNumber",
+    },
+    {
+      title: "Title",
+      dataIndex: "trackName",
+    },
+    {
+      title: "Artist",
+      dataIndex: "trackArtist",
+    },
+    {
+      title: "length",
+      dataIndex: "length",
+    },
+  ];
+  
